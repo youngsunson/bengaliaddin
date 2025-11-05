1e2a9802c6f7e416b7ba148da80047f9434e0222
Himadri Biswas <122204959+youngsunson@users.noreply.github.com>
2025-11-05 19:56:18 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/bengaliaddin/',   // <-- ADD THIS LINE
   plugins: [react()],
   server: {
     https: true,
