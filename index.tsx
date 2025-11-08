e4e7448d81d7993c02cf0b2ce3a1ef01875b06e8
Himadri Biswas <122204959+youngsunson@users.noreply.github.com>
2025-11-08 19:32:55 +0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,23 @@
 // index.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+// Wait until the Office host (Word) is ready before mounting React
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Word) {
+    const rootElement = document.getElementById("root");
+    if (!rootElement) {
+      throw new Error("Could not find root element to mount to");
+    }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  } else {
+    console.warn("This add-in is designed for Microsoft Word only.");
+  }
+});
