db8421669d7110861df010f4292ca4d10cfe2904
Himadri Biswas <122204959+youngsunson@users.noreply.github.com>
2025-11-12 21:37:30 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/bengaliaddin/',   // <-- ADD THIS LINE
   plugins: [react()],
+  base: '/', // ✅ deploys from root, fixes 404 on Netlify
   server: {
     https: true,
     port: 5173,
