cbe0119d289c17d0fb9b23d765f2e41c7de4796d
Himadri Biswas <122204959+youngsunson@users.noreply.github.com>
2025-11-12 21:37:05 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ✅ crucial for Netlify root deployment
   define: {
     'process.env': process.env,
+  },
+  server: {
+    https: true,
+    port: 5173,
   },
 });
